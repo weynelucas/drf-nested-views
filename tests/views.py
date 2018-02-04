ba36f0baf30b74761fe7fc6a15afe3b562f877f6
Lucas Weyne <weynelucas@gmail.com>
2018-02-04 19:48:26 -0300

    Create test views

--- tests/views.py
+++ tests/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from drf_nested_views import viewsets as drf_nested_viewsets
 
-# Create your views here.
+
+from . import serializers, models
+
+
+class ClientViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ClientSerializer
+    queryset = models.Client.objects.all()
+
+
+class MailDropViewSet(drf_nested_viewsets.ModelViewSet):
+    serializer_class = serializers.MailDropSerializer
+
+
+class MailRecipientViewSet(drf_nested_viewsets.ModelViewSet):
+    serializer_class = serializers.MailRecipientSerializer
